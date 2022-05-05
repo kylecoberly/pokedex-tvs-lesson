cb1eb9cfd46291d37aa195b83af82cf8e4a64910
Kyle Coberly <kyle.coberly@gmail.com>
2022-05-05 14:18:06 -0600

    Integrate with API

--- src/index.js
+++ src/index.js
@@ -1,166 +1,25 @@
-const pokemonList = [{
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}, {
-  name: "Pikachu",
-  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
-}]
-
 const $pokemonList = document.querySelector(".pokemon-list")
 
-pokemonList.map(pokemon => {
-  const $div = document.createElement("div")
-  $div.classList.add("pokemon-listing")
-  $div.innerHTML = `
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu").then(response => response.json()).then(pokemon => {
+  Array(50).fill("").map(() => {
+    console.log("here")
+    return {
+      name: titleCase(pokemon.name),
+      imageUrl: pokemon.sprites.front_default,
+    }
+  }).map(pokemon => {
+    const $div = document.createElement("div")
+    $div.classList.add("pokemon-listing")
+    $div.innerHTML = `
     <img src="${pokemon.imageUrl}" alt="${pokemon.name}" />
     <p>${pokemon.name}</p>
   `
-  return $div
-}).forEach($pokemon => {
-  console.log("here", $pokemonList, $pokemon)
-  $pokemonList.append($pokemon)
+    return $div
+  }).forEach($pokemon => {
+    $pokemonList.append($pokemon)
+  })
 })
+
+function titleCase(string) {
+  return `${string.slice(0, 1).toUpperCase()}${string.slice(1)}`
+}
